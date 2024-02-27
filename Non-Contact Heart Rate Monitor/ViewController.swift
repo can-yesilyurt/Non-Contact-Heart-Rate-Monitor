ac13457725b9a570425558b6b13e4c699372f78f
Can Yesilyurt <can--yesilyurt@hotmail.com>
2024-02-27 16:32:16 +0300

    Initial Commit

---
+++ Non-Contact Heart Rate Monitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Non-Contact Heart Rate Monitor
+//
+//  Created by Can Yesilyurt on 2/27/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
